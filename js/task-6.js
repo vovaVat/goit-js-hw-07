3e17f007d2e874485974c7873aeccd1f9501f285
Volodymyr <vovavat97@gmail.com>
2024-09-23 13:26:27 +0300

    hz

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,42 @@
+const input= document.querySelector("#controls input");
+const createButton= input.nextElementSibling;
+const destroyButton= createButton.nextElementSibling;
+const boxes= document.querySelector("#boxes");
+
+function createBoxes(amount){
+  let color;
+  let width= 30;
+  let height= 30;
+
+  boxes.innerHTML = '';
+
+  for(let i=1; i<= amount; i++){
+    color= getRandomHexColor();
+    boxes.insertAdjacentHTML("afterbegin", `<div style="width:${width}px; height:${height}px; background-color:${color};"></div>`)
+    width+=10;
+    height+=10;
+  }
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+createButton.addEventListener("click", event => {
+  const amount = parseInt(input.value);
+  if(amount <= 100){
+    createBoxes(amount);
+  }
+  input.value = '';
+});
+
+function destroyBoxes(){
+  boxes.innerHTML = '';
+}
+
+destroyButton.addEventListener("click", event => {
+  destroyBoxes();
+})
